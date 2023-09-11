6a166d1561c9a21b1d75cff33e18bac4d79e4a16
Rammiah Jeyakumar <rammiahj@gmail.com>
2023-09-11 12:35:39 +0530

    Admin template setup

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,23 @@
-<x-app-layout>
-    
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+            @include('admin.header')
+        <!-- partial -->
+            @include('admin.body')
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    @include('admin.js')
+  </body>
+</html>
